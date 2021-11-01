13ff20a1b337febe74755a71db464bd068c091e1
hua.mou <hua.mou@dmall.com>
2021-11-01 10:57:43 +0800

    'mh'

--- basic_page/base_page.py
+++ basic_page/base_page.py
@@ -1,4 +1,6 @@
 import cv2
+import win32api
+import win32con
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
