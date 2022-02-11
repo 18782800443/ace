e1e943b7886d5d6d3126f1b6c5d91ec897ce10ab
yinglong.li <yinglong.li@dmall.com>
2022-02-11 15:01:45 +0800

    alert

--- page_obj/demo/ios_demo_page.py
+++ page_obj/demo/ios_demo_page.py
@@ -145,12 +145,10 @@ class IosDemo(SnapshotPage):
         # self.get_element(LOGIN_DATA_YML, 'passwd_loc').send_keys(account)
         # self.get_element(LOGIN_DATA_YML, 'login_protocol_loc').click()
         # self.get_element(LOGIN_DATA_YML, 'login_btn_loc').click()
-        self.sort_operation(LOGIN_DATA_YML, 'allow_news_dlg', 'click')
-        time.sleep(2)
-
         self.sort_operation(LOGIN_DATA_YML, 'user_protocol_dlg', 'click')
         time.sleep(6)
-
+        self.sort_operation(LOGIN_DATA_YML, 'allow_news_dlg', 'click')
+        time.sleep(2)
         self.sort_operation(LOGIN_DATA_YML, 'account_loc', 'send_keys', value='18323721334')
         time.sleep(2)
         self.sort_operation(LOGIN_DATA_YML, 'passwd_loc', 'send_keys', value='Qmn12345')
